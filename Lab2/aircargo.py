1c95f0805018c9388c58a2df4aa6be5f2165ac24
SergZeliuk <serhiizeliuk@student.agh.edu.pl>
2025-03-18 18:05:54 +0100

    init algo

--- Lab2/aircargo.py
+++ Lab2/aircargo.py
@@ -1,13 +1,3 @@
-# stripsProblem.py - STRIPS Representations of Actions
-# AIFCA Python code Version 0.9.15 Documentation at https://aipython.org
-# Download the zip file and read aipython.pdf for documentation
-
-# Artificial Intelligence: Foundations of Computational Agents https://artint.info
-# Copyright 2017-2024 David L. Poole and Alan K. Mackworth
-# This work is licensed under a Creative Commons
-# Attribution-NonCommercial-ShareAlike 4.0 International License.
-# See: https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
-
 class Strips(object):
     def __init__(self, name, preconds, effects, cost=1):
         """
@@ -50,12 +40,40 @@ class Planning_problem(object):
         self.initial_state = initial_state
         self.goal = goal
 
+
+
+
+
 boolean = {False, True}
+
+class Plane:
+    def __init__(self, name):
+        self.name = name
+
+
+class Cargo:
+    def __init__(self, name):
+        self.name = name
+
+class Airport:
+    def __init__(self, name):
+        self.name = name
+
+
 delivery_domain = STRIPS_domain(
-    {'RLoc':{'cs', 'off', 'lab', 'mr'}, 'RHC':boolean, 'SWC':boolean,
-     'MW':boolean, 'RHM':boolean}, 
+    {'In': boolean, 'At': boolean, 'Cargo':Cargo,
+     'Plane': Plane, 'Airport': Airport}, 
+
                          #feature:values dictionary
-    { Strips('mc_cs', {'RLoc':'cs'}, {'RLoc':'off'}),   
+    {
+        
+        
+        
+        
+        
+        
+        
+         Strips('mc_cs', {'RLoc':'cs'}, {'RLoc':'off'}),   
      Strips('mc_off', {'RLoc':'off'}, {'RLoc':'lab'}),
      Strips('mc_lab', {'RLoc':'lab'}, {'RLoc':'mr'}),
      Strips('mc_mr', {'RLoc':'mr'}, {'RLoc':'cs'}),
@@ -81,52 +99,3 @@ problem2 = Planning_problem(delivery_domain,
                             {'RLoc':'lab', 'MW':True, 'SWC':True, 'RHC':False, 
                              'RHM':False}, 
                             {'SWC':False, 'MW':False, 'RHM':False})
-
-### blocks world
-def move(x,y,z):
-    """string for the 'move' action"""
-    return 'move_'+x+'_from_'+y+'_to_'+z
-def on(x):
-    """string for the 'on' feature"""
-    return x+'_is_on'
-def clear(x):
-    """string for the 'clear' feature"""
-    return 'clear_'+x
-def create_blocks_world(blocks = {'a','b','c','d'}):
-    blocks_and_table = blocks | {'table'}
-    stmap =  {Strips(move(x,y,z),{on(x):y, clear(x):True, clear(z):True}, 
-                                 {on(x):z, clear(y):True, clear(z):False})
-                    for x in blocks
-                    for y in blocks_and_table
-                    for z in blocks
-                    if x!=y and y!=z and z!=x}
-    
-    stmap.update({Strips(move(x,y,'table'), {on(x):y, clear(x):True}, 
-                                 {on(x):'table', clear(y):True})
-                    for x in blocks
-                    for y in blocks
-                    if x!=y})
-    feature_domain_dict = {on(x):blocks_and_table-{x} for x in blocks}
-    feature_domain_dict.update({clear(x):boolean for x in blocks_and_table})
-    return STRIPS_domain(feature_domain_dict, stmap)
-
-blocks1dom = create_blocks_world({'a','b','c'})
-blocks1 = Planning_problem(blocks1dom,
-     {on('a'):'table', clear('a'):True,
-      on('b'):'c',  clear('b'):True,
-      on('c'):'table', clear('c'):False}, # initial state
-     {on('a'):'b', on('c'):'a'})  #goal
-
-blocks2dom = create_blocks_world({'a','b','c','d'})
-tower4 = {clear('a'):True, on('a'):'b',
-          clear('b'):False, on('b'):'c',
-          clear('c'):False, on('c'):'d',
-          clear('d'):False, on('d'):'table'}
-blocks2 = Planning_problem(blocks2dom,
-     tower4, # initial state
-     {on('d'):'c',on('c'):'b',on('b'):'a'})  #goal
-
-blocks3 = Planning_problem(blocks2dom,
-     tower4, # initial state
-     {on('d'):'a', on('a'):'b', on('b'):'c'})  #goal
-
---
+++ Lab2/chat_approach.py
@@ -0,0 +1,136 @@
+from aircargo import Strips, STRIPS_domain, Planning_problem
+
+def create_air_cargo_domain(cargos, planes, airports):
+    """
+    Tworzy domenę STRIPS dla prostego problemu 'air cargo',
+    w którym mamy zbiór ładunków (cargos), samolotów (planes)
+    i lotnisk (airports).
+    """
+    # 1) Definiujemy słownik (feature_domain_dict) który mapuje nazwę cechy (feature)
+    #    na zbiór możliwych wartości dla tej cechy.
+    #    W tym podejściu cecha "At_X" oznacza "Gdzie jest X?", a wartości to nazwy lotnisk
+    #    lub samolotów (jeśli X to ładunek).
+    
+    feature_domain_dict = {}
+    
+    # Dla każdego ładunku definiujemy cechę "At_C" (gdzie jest cargo?),
+    # której możliwe wartości to wszystkie lotniska oraz wszystkie samoloty.
+    for c in cargos:
+        feature_domain_dict[f"At_{c}"] = set(airports) | set(planes)
+    
+    # Dla każdego samolotu definiujemy cechę "At_P", której możliwe wartości to lotniska.
+    # Zakładamy, że samolot nie "wchodzi" do innego samolotu, więc wartości to tylko lotniska.
+    for p in planes:
+        feature_domain_dict[f"At_{p}"] = set(airports)
+    
+    # 2) Definiujemy zbiór akcji (actions) w ujęciu STRIPS:
+    #    - LOAD (załaduj ładunek na samolot)
+    #    - UNLOAD (rozładuj ładunek z samolotu)
+    #    - FLY (poleć samolotem z jednego lotniska na drugie)
+
+    actions = set()
+
+    # 2A) LOAD: 
+    #   Parametry: cargo c, plane p, airport a
+    #   Prekondycje:
+    #     - ładunek c jest w miejscu a (At_{c} = a)
+    #     - samolot p jest w tym samym miejscu a (At_{p} = a)
+    #   Efekty:
+    #     - ładunek c jest teraz w samolocie p (At_{c} = p)
+    #     - tym samym ładunek nie jest już na lotnisku a
+    #
+    #   Tworzymy osobny obiekt Strips dla każdej kombinacji c, p, a.
+    for c in cargos:
+        for p in planes:
+            for a in airports:
+                name = f"LOAD_{c}_onto_{p}_at_{a}"
+                preconds = {
+                    f"At_{c}": a,  # ładunek c jest na lotnisku a
+                    f"At_{p}": a   # samolot p jest również na lotnisku a
+                }
+                effects = {
+                    f"At_{c}": p   # w efekcie ładunek c będzie w samolocie p
+                }
+                actions.add(Strips(name, preconds, effects))
+
+    # 2B) UNLOAD:
+    #   Parametry: cargo c, plane p, airport a
+    #   Prekondycje:
+    #     - ładunek c jest aktualnie w samolocie p (At_{c} = p)
+    #     - samolot p jest na lotnisku a (At_{p} = a)
+    #   Efekty:
+    #     - ładunek c jest teraz na lotnisku a (At_{c} = a)
+    #
+    #   Podobnie jak wyżej – generujemy akcje dla wszystkich możliwych kombinacji.
+    for c in cargos:
+        for p in planes:
+            for a in airports:
+                name = f"UNLOAD_{c}_from_{p}_at_{a}"
+                preconds = {
+                    f"At_{c}": p,  # ładunek c jest w samolocie p
+                    f"At_{p}": a   # samolot p jest na lotnisku a
+                }
+                effects = {
+                    f"At_{c}": a   # ładunek c trafia na lotnisko a
+                }
+                actions.add(Strips(name, preconds, effects))
+
+    # 2C) FLY:
+    #   Parametry: plane p, from, to
+    #   Prekondycje:
+    #     - samolot p znajduje się na lotnisku "from" (At_{p} = from)
+    #   Efekty:
+    #     - samolot p znajduje się teraz na lotnisku "to" (At_{p} = to)
+    #   Oczywiście "from" != "to", więc nie chcemy generować akcji FLY p z lotniska do tego samego lotniska.
+    for p in planes:
+        for fr in airports:
+            for to in airports:
+                if to != fr:
+                    name = f"FLY_{p}_from_{fr}_to_{to}"
+                    preconds = {
+                        f"At_{p}": fr  # samolot p musi być na lotnisku fr
+                    }
+                    effects = {
+                        f"At_{p}": to  # po akcji samolot jest na lotnisku to
+                    }
+                    actions.add(Strips(name, preconds, effects))
+
+    # 3) Tworzymy i zwracamy obiekt STRIPS_domain z zdefiniowanym słownikiem cech i zbiorem akcji
+    return STRIPS_domain(feature_domain_dict, actions)
+
+
+# Poniżej przykładowy problem planistyczny "na próbę",
+# w którym mamy 1 ładunek (C1), 1 samolot (P1) i 2 lotniska (SFO, JFK).
+# Cel: przetransportować ładunek C1 z lotniska SFO na lotnisko JFK.
+
+if __name__ == "__main__":
+    # 1) Definiujemy listy ładunków, samolotów i lotnisk
+    cargos = ["C1"]
+    planes = ["P1"]
+    airports = ["SFO", "JFK"]
+
+    # 2) Tworzymy domenę
+    air_cargo_domain = create_air_cargo_domain(cargos, planes, airports)
+
+    # 3) Określamy stan początkowy
+    #    - ładunek C1 jest na lotnisku SFO
+    #    - samolot P1 jest na lotnisku SFO
+    initial_state = {
+        "At_C1": "SFO",
+        "At_P1": "SFO"
+    }
+
+    # 4) Określamy cel: ładunek C1 ma być na lotnisku JFK
+    goal = {
+        "At_C1": "JFK"
+    }
+
+    # 5) Tworzymy konkretny problem planistyczny
+    air_cargo_problem = Planning_problem(air_cargo_domain, initial_state, goal)
+
+    # Na tym etapie air_cargo_problem można przekazać do algorytmu planującego
+    # (np. wyszukiwanie w przestrzeni stanów) i znajdować plan z LOAD, FLY, UNLOAD.
+    print("Domena (cechy i akcje):")
+    print("Features:", air_cargo_domain.feature_domain_dict)
+    print("Actions:", air_cargo_domain.actions)
+    print("\nProblem:\n", air_cargo_problem)
